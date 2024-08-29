3b75d6955ad29444b72b0bb2c015db28adb850bc
unknown <jankowski.r@hotmail.com>
2024-08-29 13:52:42 +0200

    Initial commit with basic structure

---
+++ src/main/java/FootballWorldCupScoreBoard.java
@@ -0,0 +1,19 @@
+public class FootballWorldCupScoreBoard {
+
+    public void startGame(Team homeTeam, Team awayTeam) {
+
+    }
+
+    public void finishGame(Game game) {
+
+    }
+
+
+    public void updateScore(Score homeTeam, Score awayTeam) {
+
+    }
+
+    public List<GameSummary> getSummary() {
+
+    }
+}
